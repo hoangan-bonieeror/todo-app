bf5ef34b23553457132dc5abc4f3691719d7ee92
Ân Nguyễn <hoangan726@gmail.com>
2022-08-16 14:36:56 +0700

    Completed Todo Service

--- src/app/services/todo.service.ts
+++ src/app/services/todo.service.ts
@@ -1,9 +1,58 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { Filter } from '../model/filtering.model';
+import { Todo } from '../model/todo.model';
+import { LocalStorageService } from './local-storage.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TodoService {
+  private static readonly TodoStorageKey = 'todos';
 
-  constructor() { }
+  private todos : Todo[] = new Array<Todo>;
+  private filteredTodos : Todo[] = new Array<Todo>;
+  private lengthSubject : BehaviorSubject<number> = new BehaviorSubject<number>(0);
+  private displayedTodosSubject : BehaviorSubject<Todo[]> = new BehaviorSubject<Todo[]>([])
+  private currentFilter : Filter = Filter.All;
+
+  todo$ : Observable<Todo[]> = this.displayedTodosSubject.asObservable();
+  length$ : Observable<number> = this.lengthSubject.asObservable();
+
+  constructor(private storageService : LocalStorageService) { }
+
+  fetchFromLocalStorage = () => {
+    this.todos = this.storageService.getValue<Todo[]>(TodoService.TodoStorageKey) || [];
+    this.filteredTodos = [...this.todos.map(todo => <Todo>({...todo}))];
+    this.pushDataToFlow();
+  } 
+
+  updateToLocalStorage = () => {
+    this.storageService.setObject(TodoService.TodoStorageKey, this.todos);
+    
+    this.pushDataToFlow();
+  }
+
+
+  filterTodos = (filterMode : Filter, isFiltering : boolean) => {
+    this.currentFilter = filterMode;
+
+    switch(filterMode) {
+      case Filter.Active :
+        this.filteredTodos = this.todos.filter(todo => !todo.isCompleted);
+        break;
+      case Filter.Completed :
+        this.filteredTodos = this.todos.filter(todo => todo.isCompleted)
+        break;
+      case Filter.All :
+        this.filteredTodos = <Todo[]>[...this.todos.map(todo => ({...todo}))];
+    }
+
+    isFiltering && this.pushDataToFlow();
+  }
+
+  private pushDataToFlow = () => {
+    this.displayedTodosSubject.next(this.filteredTodos);
+    this.lengthSubject.next(this.todos.length);
+  }
 }
